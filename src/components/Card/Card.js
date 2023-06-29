a0236d598302df7ed05e134de5d6966ca7d5b64a
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-06-28 22:40:39 -0500

    darkmode beta

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,10 @@
 import './Card.css';
 
-export default function Card({ bill_name, summary, tags, helps, hurts, showHelpsAndHurts = true }) {
+export default function Card({ bill_name, summary, tags, helps, hurts, showHelpsAndHurts = true, theme }) {
+    const cardClasses = `card ${theme === 'dark' ? 'dark' : ''}`;
+
     return (
-        <div className="card" data-aos='zoom-in'>
+        <div className={cardClasses} data-aos="zoom-in">
             <h2 className="card-name">{bill_name}</h2>
             <p className="card-summary">{summary}</p>
             <div className="card-tags">
@@ -10,22 +12,22 @@ export default function Card({ bill_name, summary, tags, helps, hurts, showHelps
                     <span key={index}>{tag}</span>
                 ))}
             </div>
-            {showHelpsAndHurts &&
+            {showHelpsAndHurts && (
                 <div className="card-helps-hurts">
-                    <div className='grouping-cards grouping-cards-negative'>
-                        <div className="card-helps-hurts-header">Negatives</div>
+                    <div className="grouping-cards grouping-cards-negative">
+                        <div className="grouping-cards-header">Negatives</div>
                         <div className="card-helps-hurts-content">
                             <p className="card-hurts">{hurts}</p>
                         </div>
                     </div>
-                    <div className='grouping-cards grouping-cards-positive'>
-                        <div className="card-helps-hurts-header">Positives</div>
+                    <div className="grouping-cards grouping-cards-positive">
+                        <div className="grouping-cards-header">Positives</div>
                         <div className="card-helps-hurts-content">
                             <p className="card-helps">{helps}</p>
                         </div>
                     </div>
                 </div>
-            }
+            )}
         </div>
     );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-// src/components/NavBar/NavBar.jsx
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import * as userService from '../../utilities/users-service';
 import './NavBar.css';
 import useBills from '../../hooks/useBills';
@@ -8,20 +8,40 @@ import useRandomBillId from '../../hooks/useRandomBillId';
 export default function NavBar({ user, setUser }) {
   const bills = useBills();
   const [randomBillId, setRandomBillId] = useRandomBillId(bills);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
 
   function handleLogOut() {
     userService.logOut();
     setUser(null);
   }
 
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  function toggleTheme() {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
+
   return (
-    <nav className="navbar" data-aos="fade-down"
-      data-aos-duration="500">
+    <nav className="navbar" data-aos="fade-down" data-aos-duration="500">
       <Link to="/" className="nav-link">Home</Link>
       <Link to="/upcoming-bills" className="nav-link">Upcoming Bills</Link>
       <Link to={`/bill/${randomBillId}`} className="nav-link" onClick={setRandomBillId}>Vote</Link>
       <span className="spacer"></span>
-      <span className="welcome-msg">Welcome, {user.name}</span>
+      <div className="user-dropdown" onClick={() => setShowDropdown(!showDropdown)}>
+        <span className="welcome-msg">Welcome, {user.name}</span>
+        {showDropdown && (
+          <div className="dropdown-options">
+            <Link to="/sample1" className="dropdown-option">Sample 1</Link>
+            <Link to="/sample2" className="dropdown-option">Sample 2</Link>
+            <button onClick={toggleTheme} className="dropdown-option">Toggle Dark Mode</button>
+          </div>
+        )}
+      </div>
       <Link to="" className="nav-link logout-link" onClick={handleLogOut}>Log Out</Link>
     </nav>
   );
